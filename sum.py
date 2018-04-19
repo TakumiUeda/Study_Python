b0f642c1900da66f3d57b3ab0f6556a3d41455b1
リイン <reinforce00111@gmail.com>
2018-04-19 15:42:35 +0900

    １−５１までの足し算を追加

---
+++ sum.py
@@ -0,0 +1,18 @@
+#coding: UTF-8
+#普通の方法
+s = 0
+for x in range(1,51):
+    s += x
+print s
+
+#sumを使う
+print sum(range(1,51))
+
+#generator式を使う
+print sum(x for x in range(1,51))
+
+#reduceを使う
+print reduce(lambda x,y: x+y, range(1,51))
+
+#頭を使う
+print 50*51/2
